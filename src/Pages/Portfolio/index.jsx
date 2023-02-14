a22d3b8cd9d357d90b993d314331c74a94147af7
Your Name <you@example.com>
2023-02-14 23:19:39 +0530

    update portfolio section

--- src/Pages/Portfolio/index.jsx
+++ src/Pages/Portfolio/index.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
-
+import React from "react";
+import "./style.scss";
+import PageHeader from "../../components/PageHeader";
+import { AiFillProject } from "react-icons/ai";
+import { filterListData, portfolioCardData } from "./portfolioData";
+import { useState } from "react";
 function Portfolio() {
+  const [hoverIndex, setHoverIndex] = useState(null);
   return (
-    <div>
-      
+    <div className="portfolio">
+      <PageHeader name="My Portfolio" icon={<AiFillProject />} />
+      <div className="portfolio-content">
+        <div className="portfolio-content-filterSection">
+          <ul>
+            {filterListData.map((item, key) => {
+              return <li key={key}>{item.label}</li>;
+            })}
+          </ul>
+        </div>
+        <div className="portfolio-content-cardContainer">
+          {portfolioCardData.map((card, key) => {
+            return (
+              <div
+                key={key}
+                className="portfolio-content-cardContainer-content"
+                onMouseEnter={() => {
+                  setHoverIndex(key);
+                }}
+                onMouseLeave={() => {
+                  setHoverIndex(null);
+                }}
+              >
+                <div className="card">
+                  <img src={card.imgUrl} alt="project_image" />
+                </div>
+                {hoverIndex === key && (
+                  <div className="cardOlay">
+                    <div>
+                    <p>{card.cardName}</p>
+                    <a href={card.projectUrl}>
+                      <button>Visit Now</button>
+                    </a>
+                    </div>
+                  </div>
+                )}
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Portfolio
+export default Portfolio;
